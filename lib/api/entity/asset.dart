8c7bdeb34f1a98f952f68fc37a8ac692b1300689
ekrem-qb <ekrem.bayram@mail.ru>
2025-07-24 18:53:00 +0300

    Rewrite `Asset` class using `freezed` Add `json_serializable` package

--- lib/api/entity/asset.dart
+++ lib/api/entity/asset.dart
@@ -1,61 +1,17 @@
-class Asset {
-  const Asset({
-    required this.id,
-    required this.rank,
-    required this.symbol,
-    required this.priceUsd,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  final String id;
-  final String rank;
-  final String symbol;
-  final String priceUsd;
+part 'asset.freezed.dart';
+part 'asset.g.dart';
 
-  factory Asset.fromJson(final Map<String, dynamic> json) {
-    return Asset(
-      id: json['id'],
-      rank: json['rank'],
-      symbol: json['symbol'],
-      priceUsd: json['priceUsd'],
-    );
-  }
+@freezed
+sealed class Asset with _$Asset {
+  const factory Asset({
+    required final String id,
+    required final String rank,
+    required final String symbol,
+    required final String priceUsd,
+  }) = _Asset;
 
-  Asset copyWith({
-    final String? id,
-    final String? rank,
-    final String? symbol,
-    final String? priceUsd,
-  }) {
-    return Asset(
-      id: id ?? this.id,
-      rank: rank ?? this.rank,
-      symbol: symbol ?? this.symbol,
-      priceUsd: priceUsd ?? this.priceUsd,
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {'id': id, 'rank': rank, 'symbol': symbol, 'priceUsd': priceUsd};
-  }
-
-  @override
-  String toString() {
-    return '''Asset(id: $id, rank: $rank, symbol: $symbol, priceUsd: $priceUsd)''';
-  }
-
-  @override
-  bool operator ==(final Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Asset &&
-        other.id == id &&
-        other.rank == rank &&
-        other.symbol == symbol &&
-        other.priceUsd == priceUsd;
-  }
-
-  @override
-  int get hashCode {
-    return id.hashCode ^ rank.hashCode ^ symbol.hashCode ^ priceUsd.hashCode;
-  }
+  factory Asset.fromJson(final Map<String, dynamic> json) =>
+      _$AssetFromJson(json);
 }

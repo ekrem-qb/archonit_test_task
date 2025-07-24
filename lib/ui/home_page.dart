da5e8cb77d14d19609d34be96ab10b96d907f381
ekrem-qb <ekrem.bayram@mail.ru>
2025-07-24 18:54:56 +0300

    Rewrite `HomePage` using `AssetsBloc`

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -2,81 +2,86 @@ import 'package:currency_formatter/currency_formatter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
-import '/api/client.dart';
+import '/bloc/assets/assets_bloc.dart';
 import '/consts.dart';
+import '/utils/result.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    final assetsFuture = context.read<ApiClient>().getAssets();
+    return const CupertinoPageScaffold(
+      child: _List(),
+    );
+  }
+}
 
-    return CupertinoPageScaffold(
-      child: FutureBuilder(
-        future: assetsFuture,
-        builder: (final context, final snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
+class _List extends StatelessWidget {
+  const _List();
 
-          if (!snapshot.hasData) {
-            return const Center(child: CupertinoActivityIndicator());
-          } else {
-            final assets = snapshot.data!;
+  @override
+  Widget build(final BuildContext context) {
+    final (_, count, bloc) = context.select(
+      (final AssetsBloc bloc) =>
+          (bloc.state.assets.runtimeType, bloc.state.totalCount, bloc),
+    );
 
-            return ListView.builder(
-              itemCount: assets.length,
-              itemBuilder: (final context, final index) {
-                final asset = assets[index];
-                return Padding(
-                  padding: const EdgeInsets.all(14),
-                  child: Row(
-                    children: [
-                      Opacity(
-                        opacity: 0.1,
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            color: CupertinoColors.systemRed,
-                            borderRadius: BorderRadius.circular(18),
-                          ),
-                          child: const SizedBox.square(dimension: 56),
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: DefaultTextStyle(
-                            style: const TextStyle(
-                              fontFamily: 'SF Pro Text',
-                              fontSize: 17,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: -0.41,
-                              color: Color(0xFF17171A),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(asset.symbol),
-                                Text(
-                                  CurrencyFormatter.format(
-                                    asset.priceUsd,
-                                    kCurrencyFormat,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
+    return switch (bloc.state.assets) {
+      Loading() => const Center(
+        child: CupertinoActivityIndicator(),
+      ),
+      Error(:final error) => Center(child: Text(error.toString())),
+      Success(:final data) => ListView.builder(
+        itemCount: count,
+        itemBuilder: (final context, final index) {
+          final asset = data[index];
+
+          return Padding(
+            padding: const EdgeInsets.all(14),
+            child: Row(
+              children: [
+                Opacity(
+                  opacity: 0.1,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: CupertinoColors.systemRed,
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: const SizedBox.square(dimension: 56),
                   ),
-                );
-              },
-            );
-          }
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontFamily: 'SF Pro Text',
+                        fontSize: 17,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: -0.41,
+                        color: Color(0xFF17171A),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(asset.symbol),
+                          Text(
+                            CurrencyFormatter.format(
+                              asset.priceUsd,
+                              kCurrencyFormat,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
         },
       ),
-    );
+    };
   }
 }
